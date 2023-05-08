b04f2bbd335a271067f34da6da0402ad62bf0cbd
sharifthettu <sharif55167849@gmail.com>
2023-05-08 09:40:58 +0530

    techhub

---
+++ txtwrap.py
@@ -0,0 +1,21 @@
+# Text Wrap in Python:
+
+# Without using 'textwrap' Module:
+
+string, max_width = input(),int(input())
+for i in range(0, len(string), max_width):
+    print(string[i : i + max_width])
+
+#######################################################################################################################################
+
+# Alternate Solution: This is for HackerRank Solution which is using "textwrap" module:
+
+import textwrap
+
+def wrap(string, max_width):
+    return textwrap.fill(string,max_width)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
